477b5c85a5df2ec5e43d6f46edff7a62896680a5
WinsonWu <awinsonwu@gmail.com>
2018-05-27 08:30:35 +0800

    config

--- config-server/src/main/webapp/index.jsp
+++ config-server/src/main/webapp/index.jsp
@@ -1,10 +1,4 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: awins
-  Date: 2018/5/26
-  Time: 23:56
-  To change this template use File | Settings | File Templates.
---%>
+
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
   <head>
--- consumer/src/main/java/tech/winson/eureka/controller/HttpController.java
+++ consumer/src/main/java/tech/winson/eureka/controller/HttpController.java
@@ -2,6 +2,7 @@ package tech.winson.eureka.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -34,6 +35,14 @@ public class HttpController {
         StringBuffer stringBuffer=new StringBuffer();
         stringBuffer.append("[Ribbon]").append(helloRibbonService.hello(name));
         return stringBuffer.toString();
+    }
+
+    @Value("${foo}")
+    String foo;
+    @RequestMapping("/helloConfig")
+    @ResponseBody
+    public String helloConfig() {
+        return foo;
     }
 
 }
